326a44dd6e4d1b21a370479df947b0d717d3ab32
Peter Gomes <pgomes3@student.fitchburgstate.edu>
2022-04-07 21:27:35 -0400

    Settings Menu Created

--- Assets/Scripts/TitlescreenController.cs
+++ Assets/Scripts/TitlescreenController.cs
@@ -6,16 +6,37 @@ using UnityEngine.SceneManagement;
 public class TitlescreenController : MonoBehaviour
 {
     [SerializeField] private string levelToLoad;
+    [SerializeField] private GameObject[] menuStates;
+    private enum MenuState { TITLESCREEN, PREFERENCES };
+    private MenuState currentMenuState;
 
     private void Start()
     {
         FindObjectOfType<AudioManager>().Play("Titlescreen", 1);
+        currentMenuState = MenuState.TITLESCREEN;
     }
 
     public void StartGame()
     {
         FindObjectOfType<AudioManager>().Stop("Titlescreen");
         SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void TogglePreferences()
+    {
+        switch (currentMenuState)
+        {
+            case MenuState.TITLESCREEN:
+                menuStates[0].SetActive(false);
+                menuStates[1].SetActive(true);
+                currentMenuState = MenuState.PREFERENCES;
+                break;
+            case MenuState.PREFERENCES:
+                menuStates[0].SetActive(true);
+                menuStates[1].SetActive(false);
+                currentMenuState = MenuState.TITLESCREEN;
+                break;
+        }
     }
 
     public void QuitGame()
